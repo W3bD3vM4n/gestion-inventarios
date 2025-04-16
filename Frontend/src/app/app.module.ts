8690fcbd0634f42c2fdfe74cb5951517edc3c7f0
Hassan Rahman <hr-awesome@hotmail.com>
2025-04-16 17:05:15 -0500

    Update PrimeNG dependencies for table usage

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+
+import { TableModule } from 'primeng/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,10 +38,12 @@ import { HomeComponent } from './componentes/home/home.component';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         AppRoutingModule,
         HttpClientModule,
         ReactiveFormsModule,
-        FormsModule
+        FormsModule,
+        TableModule
     ],
     providers: [],
     bootstrap: [AppComponent]
